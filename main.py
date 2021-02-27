52a840991a07d9c48f224e05c99269b371b66033
jamba777 <liteon0206@gmail.com>
2021-02-27 14:17:55 +0200

    new main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import config
+def open_file():
+    numbers = [random.randint(1, 100) for x in range(1, 50)]
+    numbers.sort()
+    with open("text.txt", "w") as f:
+        f.write(f'{numbers}')
+    with open("text.txt", "r") as x:
+        num = x.read()
+    return num
 
-print(config.login)
+print(open_file())
